36d90828c3e5e083c2f19252ae7d4d51b8af119b
torstein <t.soernes@gmail.com>
2019-02-15 16:35:24 +0100

    faster 'pythagorean triplets'

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -2,9 +2,11 @@ use std::collections::HashSet;
 
 pub fn find(n: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
+    // a < b < c
     for b in 1..n {
-        for a in 1..b.min(n - b) {
-            let c = n - a - b;
+        let nb = n - b;
+        for a in 1..b.min(nb) {
+            let c = nb - a;
             if a * a + b * b == c * c {
                 triplets.insert([a, b, c]);
             }
