84933021c0d4e038c393b10371c4da4b88478414
torstein <t.soernes@gmail.com>
2019-02-17 16:28:50 +0100

    [luhn] simplify

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -6,12 +6,14 @@ pub fn is_valid(code: &str) -> bool {
     }
     let mut cnums: Vec<u32> = Vec::with_capacity(code.len());
     for ch in code.chars() {
-        let num = ch as i32 - 48;
+        let num = ch as i32 - 48; // '0' -> 0, '1' -> 1, etc..
         if num < 0 || num > 9 {
+            // character is not a digit, hence code is invalid
             return false;
         }
         cnums.push(num as u32);
     }
+
     for cnum in cnums.iter_mut().rev().skip(1).step_by(2) {
         *cnum *= 2;
         if *cnum > 9 {
@@ -19,7 +21,5 @@ pub fn is_valid(code: &str) -> bool {
         }
     }
     let sum: u32 = cnums.iter().sum();
-    let valid = (sum % 10) == 0;
-    println!("{:?}, {:?}, {}, {}", code, cnums, sum, valid);
-    valid
+    (sum % 10) == 0
 }
