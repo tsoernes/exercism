6cd768205105acb3912afdc5ecee681c22d90312
torstein <t.soernes@gmail.com>
2019-02-17 14:18:05 +0100

    [saddle points] simplify

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,7 +1,3 @@
-// It's called a "saddle point" because it is greater than or equal to every
-// element in its row and less than or equal to every element in its
-// column.
-// At most 1 saddle point per row (the max of the row)
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let n_rows = input.len();
     if n_rows == 0 {
@@ -11,41 +7,31 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     if n_cols == 0 {
         return vec![];
     }
-    println!("input:");
-    for row in input.iter() {
-        println!("{:?}", row);
-    }
 
-    let mut rows_maxes = Vec::with_capacity(n_rows);
-    for (_, row) in input.iter().enumerate() {
+    // For each row, a vector with the indecies of the max element in that row
+    let mut rows_maxes: Vec<Vec<usize>> = Vec::with_capacity(n_rows);
+    for row in input.iter() {
         let max = row.iter().max().unwrap();
-        let amaxes = indecies_of(row.iter(), &max);
+        let amaxes = (0..n_cols).filter(|col| row[*col] == *max).collect();
         rows_maxes.push(amaxes);
     }
-    println!("row maxes {:?}", rows_maxes);
-    let mut cols_mins = Vec::with_capacity(n_cols);
-    for i in 0..n_cols {
-        let mut min = input[0][i];
-        for j in 1..n_rows {
-            if input[j][i] < min {
-                min = input[j][i];
-            }
-        }
-        let mut amins = vec![];
-        for j in 0..n_rows {
-            if input[j][i] == min {
-                amins.push(j);
-            }
-        }
+
+    // For each col, a vector with the indecies of the min element in that col
+    let mut cols_mins: Vec<Vec<usize>> = Vec::with_capacity(n_cols);
+    for col in 0..n_cols {
+        let min = (0..n_rows).map(|row| input[row][col]).min().unwrap();
+        let amins = (0..n_rows).filter(|row| input[*row][col] == min).collect();
         cols_mins.push(amins);
     }
-    println!("col mins {:?}", cols_mins);
+
     let mut saddles = vec![];
     for (row, row_maxes) in rows_maxes.iter().enumerate() {
         for row_max_col in row_maxes.iter() {
-            for (col, col_mins) in cols_mins.get(*row_max_col).iter().enumerate() {
+            for col_mins in cols_mins.get(*row_max_col).iter() {
                 for col_min_row in col_mins.iter() {
                     if (*col_min_row) == row {
+                        // The point is simultaneously the max of its
+                        // row and the min of its column, i.e. a saddle point
                         saddles.push((row, *row_max_col));
                     }
                 }
@@ -55,18 +41,21 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     saddles
 }
 
-fn indecies_of<I: Iterator>(i: I, e: &I::Item) -> Vec<usize>
-where
-    I::Item: Eq,
-{
-    let mut ixs = vec![];
-    for (i, x) in i.enumerate() {
-        if &x == e {
-            ixs.push(i)
-        }
-    }
-    ixs
-}
+// /// Given an iterator and an element, return a Vec of indecies
+// /// of that element as they are encountered in the iterator
+// fn indecies_of<I>(i: I, e: &I::Item) -> Vec<usize>
+// where
+//     I: Iterator,
+//     I::Item: Eq,
+// {
+//     let mut ixs = vec![];
+//     for (i, x) in i.enumerate() {
+//         if &x == e {
+//             ixs.push(i)
+//         }
+//     }
+//     ixs
+// }
 // what to do if multiple indexes has same value as argmax?
 // fn argmax<I: Iterator>(i: I) -> Option<usize>
 // where
