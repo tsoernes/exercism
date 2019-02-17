862336c79c1e55a09318df116404e53b9b8ff316
torstein <t.soernes@gmail.com>
2019-02-17 20:47:49 +0100

    [Circular buffer] Avoid vector insertions at the cost of Default trait constraint

--- rust/circular-buffer/src/lib.rs
+++ rust/circular-buffer/src/lib.rs
@@ -1,8 +1,12 @@
+use std::mem;
+
 pub struct CircularBuffer<T> {
     // The oldest element, if any, is at index 'oldest'. The age of the elements
     // increase to the right of 'oldest' (and wraps around if 'oldest' > 0).
     buffer: Vec<T>,
     oldest: usize,
+    next: usize,
+    is_empty: bool,
 }
 
 #[derive(Debug, PartialEq)]
@@ -11,51 +15,65 @@ pub enum Error {
     FullBuffer,
 }
 
-impl<T> CircularBuffer<T> {
+impl<T> CircularBuffer<T>
+where
+    T: Default,
+{
     pub fn new(capacity: usize) -> Self {
+        let mut zeros = Vec::with_capacity(capacity);
+        for _ in 0..capacity {
+            zeros.push(Default::default());
+        }
         CircularBuffer {
-            buffer: Vec::with_capacity(capacity),
+            buffer: zeros,
             oldest: 0,
+            next: 0,
+            is_empty: true,
         }
     }
 
     fn is_full(&self) -> bool {
-        self.buffer.len() == self.buffer.capacity()
+        self.oldest == self.next && !self.is_empty
     }
 
     pub fn write(&mut self, element: T) -> Result<(), Error> {
         if self.is_full() {
             return Err(Error::FullBuffer);
         }
-        self.buffer.push(element);
+        self.is_empty = false;
+        self.buffer[self.next] = element;
+        self.next = (self.next + 1) % self.buffer.len();
         Ok(())
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        if self.buffer.is_empty() {
+        if self.is_empty {
             return Err(Error::EmptyBuffer);
         }
-        let element = self.buffer.remove(self.oldest);
-        // Vec::remove automatically shifts elements to the left,
-        // thus 'oldest' does not need to be changed unless we just
-        // popped the last element of the vector.
-        if self.oldest == self.buffer.len() {
-            self.oldest = 0;
+        let mut empty = Default::default();
+        mem::swap(&mut empty, self.buffer.get_mut(self.oldest).unwrap());
+        self.oldest = (self.oldest + 1) % self.buffer.len();
+        if self.oldest == self.next {
+            self.is_empty = true;
         }
-        Ok(element)
+        Ok(empty)
     }
 
     pub fn clear(&mut self) {
+        for i in 0..self.buffer.capacity() {
+            self.buffer[i] = Default::default();
+        }
         self.oldest = 0;
-        self.buffer.clear();
+        self.next = 0;
+        self.is_empty = true
     }
 
     pub fn overwrite(&mut self, element: T) {
+        self.is_empty = false;
+        self.buffer[self.next] = element;
         if self.is_full() {
-            self.buffer[self.oldest] = element;
             self.oldest = (self.oldest + 1) % self.buffer.capacity();
-        } else {
-            self.buffer.push(element);
         }
+        self.next = (self.next + 1) % self.buffer.len();
     }
 }
