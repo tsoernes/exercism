5b4ba1472f1d83bf075e72a65f24bcbadadaede0
torstein <t.soernes@gmail.com>
2019-02-17 01:45:30 +0100

    [pyth triplets] Use rayon; lower range bound

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,4 @@
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::collections::HashSet;
 
 pub fn find(n: u32) -> HashSet<[u32; 3]> {
@@ -11,19 +12,25 @@ pub fn find(n: u32) -> HashSet<[u32; 3]> {
     // 0 = n^2 - 2na - 2nb + 2ab
     // b (2n - 2a) = n^2 - 2na
     // b  = (n^2 - 2na) / (2n - 2a)
-    let triplets = (1..n)
-        .into_iter()
-        .map(|a| {
-            let t1 = n.pow(2) - 2 * n * a;
+    let t0: u32 = n.pow(2);
+    let triplets = (1..n / 3)
+        .into_par_iter()
+        .filter_map(|a| {
+            let t1 = t0 - 2 * n * a;
             let t2 = 2 * n - 2 * a;
             let b = t1 / t2;
             let c = n - a - b;
-            [a, b, c]
+            if a.pow(2) + b.pow(2) == c.pow(2) {
+                Some([a, b, c])
+            } else {
+                None
+            }
         })
-        // Stop when 'a' becomes larger or equal to 'b'
+        // Stop when 'a' becomes larger than or equal to 'b'
         // to avoid redundant triplet permutations
-        .take_while(|trip| trip[0] < trip[1])
-        .filter(|trip| trip[0].pow(2) + trip[1].pow(2) == trip[2].pow(2))
+        .map(|trip| if trip[0] < trip[1] { Some(trip) } else { None })
+        .while_some()
         .collect();
+
     triplets
 }
