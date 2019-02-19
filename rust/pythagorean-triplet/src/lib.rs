c55eb57617415262175e770d2403f492eb43c4fc
torstein <t.soernes@gmail.com>
2019-02-19 10:02:32 +0100

    [Pyth triplets] Improve variable naming

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,8 +1,8 @@
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::collections::HashSet;
 
-pub fn find(n: u32) -> HashSet<[u32; 3]> {
-    // Given:
+pub fn find(sum: u32) -> HashSet<[u32; 3]> {
+    // Given: (where n=sum)
     // a < b < c
     // a + b + c = n
     // a^2 + b^2 = c^2
@@ -12,23 +12,28 @@ pub fn find(n: u32) -> HashSet<[u32; 3]> {
     // 0 = n^2 - 2na - 2nb + 2ab
     // b (2n - 2a) = n^2 - 2na
     // b  = (n^2 - 2na) / (2n - 2a)
-    let t0: u32 = n.pow(2);
-    let triplets = (1..n / 3)
+    let sum_pow2: u32 = sum.pow(2);
+    let triplets = (1..sum / 3)
         .into_par_iter()
-        .filter_map(|a| {
-            let t1 = t0 - 2 * n * a;
-            let t2 = 2 * n - 2 * a;
-            let b = t1 / t2;
-            let c = n - a - b;
-            if a.pow(2) + b.pow(2) == c.pow(2) {
-                Some([a, b, c])
+        .filter_map(|side_a| {
+            let side_b = (sum_pow2 - 2 * sum * side_a) / (2 * sum - 2 * side_a);
+            let side_c = sum - side_a - side_b;
+            let is_valid_triangle = side_a.pow(2) + side_b.pow(2) == side_c.pow(2);
+            if is_valid_triangle {
+                Some([side_a, side_b, side_c])
             } else {
                 None
             }
         })
         // Stop when 'a' becomes larger than or equal to 'b'
         // to avoid redundant triplet permutations
-        .map(|trip| if trip[0] < trip[1] { Some(trip) } else { None })
+        .map(|sides| {
+            if sides[0] < sides[1] {
+                Some(sides)
+            } else {
+                None
+            }
+        })
         .while_some()
         .collect();
 
