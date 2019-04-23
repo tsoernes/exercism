72def28bfdb00c8c5a8609e2e0d5b032a38c09bb
torstein <t.soernes@gmail.com>
2019-04-24 01:57:54 +0200

    Improve atbash-cipher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,45 +1,43 @@
-const FIRST: u8 = 'a' as u8;
-const LAST: u8 = 'z' as u8;
+const FIRST: u8 = b'a';
+const LAST: u8 = b'z';
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let plain = plain
+    let mut chars_pushed = 0;
+    plain
         .replace(".", "")
         .replace(",", "")
         .replace(" ", "")
-        .to_lowercase();
-    let n_spaces = plain.len() % 5;
-    let mut enc: Vec<u8> = Vec::with_capacity(plain.len() + n_spaces);
-    let mut chars_pushed = 0;
-    for ch in plain.chars() {
-        if chars_pushed > 0 && chars_pushed % 5 == 0 {
-            enc.push(' ' as u8);
-        }
-        let mut ch_n = ch as u8;
-        if ch_n >= FIRST && ch_n <= LAST {
-            ch_n = FIRST + LAST - ch_n;
-        }
-        if ch_n < 128 {
+        .to_lowercase()
+        .chars()
+        .filter(|x| x.is_ascii())
+        .flat_map(|ch| {
+            let mut res = vec![];
+            if chars_pushed > 0 && chars_pushed % 5 == 0 {
+                res.push(' ');
+            }
+            let mut ch_n = ch as u8;
+            if ch.is_ascii_lowercase() {
+                ch_n = FIRST + LAST - ch_n;
+            }
             chars_pushed += 1;
-            enc.push(ch_n);
-        }
-    }
-    String::from_utf8(enc).unwrap()
+            res.push(ch_n as char);
+            res
+        })
+        .collect()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    let dec: Vec<u8> = cipher
+    cipher
         .replace(" ", "")
         .chars()
         .map(|ch| {
-            let ch_n = ch as u8;
-            if ch_n >= FIRST && ch_n <= LAST {
-                FIRST + LAST - ch_n
+            if ch.is_ascii_lowercase() {
+                (FIRST + LAST - ch as u8) as char
             } else {
-                ch_n
+                ch
             }
         })
-        .collect();
-    String::from_utf8(dec).unwrap()
+        .collect()
 }
