fdcafee1206ca64bfc59ac7a7a72dea8077ea254
Torstein Sørnes <t.soernes@gmail.com>
2023-03-08 21:02:01 +0100

    [saddle-points] fix bug

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -10,9 +10,10 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    const ORBITAL_SECS: f64;
+    const ORBITAL_YEARS: f64;
+    const EARTH_ORBITAL_SECS: f64 = 31557600.0;
     fn years_during(d: &Duration) -> f64 {
-        (d.seconds as f64) / Self::ORBITAL_SECS
+        (d.seconds as f64) / (Self::ORBITAL_YEARS * Self::EARTH_ORBITAL_SECS)
     }
 }
 
@@ -25,29 +26,27 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-const EARTH_ORBITAL_SECS: f64 = 31557600.0;
-
 impl Planet for Mercury {
-    const ORBITAL_SECS: f64 = 0.2408467 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 0.2408467;
 }
 impl Planet for Venus {
-    const ORBITAL_SECS: f64 = 0.6151972 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 0.6151972;
 }
 impl Planet for Earth {
-    const ORBITAL_SECS: f64 = 1.0000000 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 1.0000000;
 }
 impl Planet for Mars {
-    const ORBITAL_SECS: f64 = 1.8808158 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 1.8808158;
 }
 impl Planet for Jupiter {
-    const ORBITAL_SECS: f64 = 11.862615 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 11.862615;
 }
 impl Planet for Saturn {
-    const ORBITAL_SECS: f64 = 29.447498 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 29.447498;
 }
 impl Planet for Uranus {
-    const ORBITAL_SECS: f64 = 84.016846 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 84.016846;
 }
 impl Planet for Neptune {
-    const ORBITAL_SECS: f64 = 164.79132 * EARTH_ORBITAL_SECS;
+    const ORBITAL_YEARS: f64 = 164.79132;
 }
