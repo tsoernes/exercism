3b60cc2a8d8158f0e4c035f17ad9ad454a41254c
Torstein Sørnes <t.soernes@gmail.com>
2023-03-08 22:25:27 +0100

    [clock] nearly finished]

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,37 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut hours = (hours + minutes / 60).rem_euclid(24);
+        if minutes < 0 {
+            hours = (hours - 1).rem_euclid(24)
+        }
+        let minutes = minutes.rem_euclid(60);
+        Clock {
+            hours: hours,
+            minutes: minutes,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
+    }
+}
